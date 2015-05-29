8e979ae0ae94a5cb1e3b641869246997d7fc8f82
Egor Smolyakov <egorsmkv@gmail.com>
2015-05-29 17:01:23 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dockerfile-parser',
+    keywords='docker dockerfile parser parsing',
+    version='0.0.1',
+    url='https://github.com/eg0r/dockerfile-parser',
+    license='MIT',
+    author='Egor Smolyakov',
+    author_email='egorsmkv@gmail.com',
+    description='Library for parsing Dockerfile.',
+    zip_safe=False,
+    packages=find_packages(),
+    py_modules=['dockerfile_parse'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
